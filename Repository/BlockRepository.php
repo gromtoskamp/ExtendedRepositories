8a70f78b5bc5fe4a9d97323df171c38e471500b9
gromtoskamp <tom.groskamp@gmail.com>
2020-07-18 12:42:15 +0200

    extend original repositories

--- Repository/BlockRepository.php
+++ Repository/BlockRepository.php
@@ -3,26 +3,45 @@
 namespace Groskampweb\ExtendedRepositories\Repository;
 
 use Groskampweb\ExtendedRepositories\Search\IdentifierCriteriaBuilder;
+use Magento\Cms\Api\Data;
 use Magento\Cms\Api\Data\BlockInterface;
+use Magento\Cms\Model\BlockFactory;
 use Magento\Cms\Model\BlockRepository as MagentoBlockRepository;
+use Magento\Cms\Model\ResourceModel\Block as ResourceBlock;
+use Magento\Cms\Model\ResourceModel\Block\CollectionFactory as BlockCollectionFactory;
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
+use Magento\Framework\Reflection\DataObjectProcessor;
+use Magento\Store\Model\StoreManagerInterface;
 
-class BlockRepository
+class BlockRepository extends MagentoBlockRepository
 {
-    /** @var MagentoBlockRepository */
-    private $blockRepository;
     /** @var IdentifierCriteriaBuilder */
     private $identifierCriteriaBuilder;
 
-    /**
-     * BlockRepository constructor.
-     * @param MagentoBlockRepository $blockRepository
-     * @param IdentifierCriteriaBuilder $identifierCriteriaBuilder
-     */
     public function __construct(
-        MagentoBlockRepository $blockRepository,
-        IdentifierCriteriaBuilder $identifierCriteriaBuilder
+        ResourceBlock $resource,
+        BlockFactory $blockFactory,
+        Data\BlockInterfaceFactory $dataBlockFactory,
+        BlockCollectionFactory $blockCollectionFactory,
+        Data\BlockSearchResultsInterfaceFactory $searchResultsFactory,
+        DataObjectHelper $dataObjectHelper,
+        DataObjectProcessor $dataObjectProcessor,
+        StoreManagerInterface $storeManager,
+        IdentifierCriteriaBuilder $identifierCriteriaBuilder,
+        CollectionProcessorInterface $collectionProcessor = null
     ) {
-        $this->blockRepository = $blockRepository;
+        parent::__construct(
+            $resource,
+            $blockFactory,
+            $dataBlockFactory,
+            $blockCollectionFactory,
+            $searchResultsFactory,
+            $dataObjectHelper,
+            $dataObjectProcessor,
+            $storeManager,
+            $collectionProcessor
+        );
         $this->identifierCriteriaBuilder = $identifierCriteriaBuilder;
     }
 
@@ -33,7 +52,7 @@ class BlockRepository
     public function getByIdentifier(string $identifier): array
     {
         $searchCriteria = $this->identifierCriteriaBuilder->create($identifier);
-        $blockSearchResults = $this->blockRepository->getList($searchCriteria);
+        $blockSearchResults = $this->getList($searchCriteria);
 
         return $blockSearchResults->getItems();
     }
--- Repository/PageRepository.php
+++ Repository/PageRepository.php
@@ -3,10 +3,19 @@
 namespace Groskampweb\ExtendedRepositories\Repository;
 
 use Groskampweb\ExtendedRepositories\Search\IdentifierCriteriaBuilder;
+use Magento\Cms\Api\Data;
 use Magento\Cms\Api\Data\PageInterface;
+use Magento\Cms\Model\Page\IdentityMap;
+use Magento\Cms\Model\PageFactory;
 use Magento\Cms\Model\PageRepository as MagentoPageRepository;
+use Magento\Cms\Model\ResourceModel\Page as ResourcePage;
+use Magento\Cms\Model\ResourceModel\Page\CollectionFactory as PageCollectionFactory;
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
+use Magento\Framework\Reflection\DataObjectProcessor;
+use Magento\Store\Model\StoreManagerInterface;
 
-class PageRepository
+class PageRepository extends MagentoPageRepository
 {
     /** @var MagentoPageRepository */
     private $pageRepository;
@@ -15,15 +24,30 @@ class PageRepository
 
     /**
      * PageRepository constructor.
-     * @param MagentoPageRepository $pageRepository
-     * @param IdentifierCriteriaBuilder $identifierCriteriaBuilder
+     * @param ResourcePage $resource
+     * @param PageFactory $pageFactory
+     * @param Data\PageInterfaceFactory $dataPageFactory
+     * @param PageCollectionFactory $pageCollectionFactory
+     * @param Data\PageSearchResultsInterfaceFactory $searchResultsFactory
+     * @param DataObjectHelper $dataObjectHelper
+     * @param DataObjectProcessor $dataObjectProcessor
+     * @param StoreManagerInterface $storeManager
+     * @param CollectionProcessorInterface|null $collectionProcessor
+     * @param IdentityMap|null $identityMap
      */
     public function __construct(
-        MagentoPageRepository $pageRepository,
-        IdentifierCriteriaBuilder $identifierCriteriaBuilder
+        ResourcePage $resource,
+        PageFactory $pageFactory,
+        Data\PageInterfaceFactory $dataPageFactory,
+        PageCollectionFactory $pageCollectionFactory,
+        Data\PageSearchResultsInterfaceFactory $searchResultsFactory,
+        DataObjectHelper $dataObjectHelper,
+        DataObjectProcessor $dataObjectProcessor,
+        StoreManagerInterface $storeManager,
+        CollectionProcessorInterface $collectionProcessor = null,
+        ?IdentityMap $identityMap = null
     ) {
-        $this->pageRepository = $pageRepository;
-        $this->searchCriteriaBuilder = $identifierCriteriaBuilder;
+        parent::__construct($resource, $pageFactory, $dataPageFactory, $pageCollectionFactory, $searchResultsFactory, $dataObjectHelper, $dataObjectProcessor, $storeManager, $collectionProcessor, $identityMap);
     }
 
     /**
