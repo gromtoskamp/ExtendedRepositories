f31a176e512bb21e2d55c61b4fb35f7562e0ede1
gromtoskamp <tom.groskamp@gmail.com>
2020-07-24 13:11:54 +0200

    fix PageRepository

--- Repository/PageRepository.php
+++ Repository/PageRepository.php
@@ -17,8 +17,6 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class PageRepository extends MagentoPageRepository
 {
-    /** @var MagentoPageRepository */
-    private $pageRepository;
     /** @var IdentifierCriteriaBuilder */
     private $searchCriteriaBuilder;
 
@@ -45,9 +43,11 @@ class PageRepository extends MagentoPageRepository
         DataObjectProcessor $dataObjectProcessor,
         StoreManagerInterface $storeManager,
         CollectionProcessorInterface $collectionProcessor = null,
+        IdentifierCriteriaBuilder $identifierCriteriaBuilder,
         ?IdentityMap $identityMap = null
     ) {
         parent::__construct($resource, $pageFactory, $dataPageFactory, $pageCollectionFactory, $searchResultsFactory, $dataObjectHelper, $dataObjectProcessor, $storeManager, $collectionProcessor, $identityMap);
+        $this->searchCriteriaBuilder = $identifierCriteriaBuilder;
     }
 
     /**
@@ -57,7 +57,7 @@ class PageRepository extends MagentoPageRepository
     public function getByIdentifier(string $identifier): array
     {
         $identifierFilter = $this->searchCriteriaBuilder->create($identifier);
-        $pageSearchResults = $this->pageRepository->getList($identifierFilter);
+        $pageSearchResults = $this->getList($identifierFilter);
 
         return $pageSearchResults->getItems();
     }
